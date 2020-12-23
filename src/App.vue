2ea88ecba652210588d59e6ef2965f236f4b8671
frontov <roman@frontov.ru>
2020-12-23 17:16:24 +0300

    init commit 1

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+
+  <div id="app">
+
+    <H1>Hello or welcome!</H1>
+
+  </div>
+
+</template>
+
+<script>
+
+export default {
+  name: 'App',
+  components: {}
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
